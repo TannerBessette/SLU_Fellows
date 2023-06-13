671388551d2fb0d0180d34dbee4b87337430a685
Tanner Bessette <tjbsoccerguy3@gmail.com>
2023-06-13 15:43:17 -0400

    Classifying winner_factor as a factor, made a random forest model with only predictors that players are in full control of, used here in loading in dataset in RL_write_up

--- RL_Notebooks/RL_Data_Cleaning.R
+++ RL_Notebooks/RL_Data_Cleaning.R
@@ -8,7 +8,6 @@ library(dplyr)
 # read in the team dataset
 rl_df <- read.csv("~/Desktop/SLU_Fellows/RL_Datasets/games_by_teams.csv")
 
-
 # Create a dataset with all of the diff variables
 team_diff <- rl_df |> pivot_wider(id_cols = game_id, 
                                   names_from = color,
@@ -24,16 +23,13 @@ team_diff <- rl_df |> pivot_wider(id_cols = game_id,
   select(game_id, team_id_blue, team_id_orange, winner_numeric, 
          ends_with("_diff"), everything())
 
-
 # Removing "blue" and "orange" from all of the variable names:
 old_names <- names(team_diff)
 new_names <- str_remove(old_names, pattern = "_blue") 
 names(team_diff) <- new_names
 
-
 # Remove some of the unnecessary variables to tidy the dataset:
 team_diff <- team_diff |> select(-c(59:154))
-
 
 # Remove variables that are identical to another variable or a linear 
 # combination of other variables in the dataset (and goal_diff):
@@ -44,27 +40,22 @@ team_diff <- team_diff |> select(-c("boost_amount_collected_diff",
                                     "demo_taken_diff",
                                     "winner_orange"))
 
-
 # create a winner_factor variable that can be used for classification problems
 team_diff <- team_diff |> mutate(winner_factor = 
-                                   factor(winner_numeric, levels = c(1, 0)))
-
+                                   as.factor(as.character(winner_numeric)))
 
 # only keep rows where shooting percentage difference is less than absolute 
 # value of 100, because it is not possible for anything to be outside of this
 team_diff <- team_diff |> filter(core_shooting_percentage_diff >= -100)
 team_diff <- team_diff |> filter(core_shooting_percentage_diff <= 100)
 
-
 # Remove any NA values:
 team_diff <- na.omit(team_diff)
-
 
 # To make train/test split more fair, keep all series in one or the other,
 # not both, by creating a series_id
 team_diff <- team_diff |> 
   mutate(series_id = paste(team_id, team_id_orange, sep = "_"))
-
 
 # reorder the dataset so all of the non-numeric variables come before numeric:
 team_diff <- team_diff |>
@@ -103,7 +94,6 @@ RL_joined <- left_join(team_diff, team_std, by = c("game_id" = "game_id"))
 # Remove rows with NAs from the dataset
 RL_joined <- na.omit(RL_joined)
 
-
 # Output a csv with the joined team and players datasets:
 # (this should include all of the diff variables and all of the sd variables)
 write_csv(x = RL_joined, "~/Desktop/SLU_Fellows/RL_Datasets/RL_joined.csv")
@@ -118,7 +108,6 @@ write_csv(x = RL_joined, "~/Desktop/SLU_Fellows/RL_Datasets/RL_joined.csv")
 # Only keep winner and variables that are numeric:
 RL_numeric <- RL_joined %>% select(where(is.numeric), series_id, winner_factor)
 
-
 # Remove some numeric predictors that have obvious correlation to game outcome:
 RL_numeric <- RL_numeric %>% select(-c("core_goals_diff",
                                        "core_assists_diff",
@@ -126,7 +115,6 @@ RL_numeric <- RL_numeric %>% select(-c("core_goals_diff",
                                        # of winner numeric
                                        "winner_numeric",
                                        "core_score_diff"))
-
 
 # Create a csv file for RL_numeric dataset:
 write_csv(x = RL_numeric, "~/Desktop/SLU_Fellows/RL_Datasets/RL_numeric.csv")
